161e355bc9647b93ce8a6520c802ac5e72cd29a1
Ghelyaei <ghelyaei97@gmail.com>
2023-01-19 21:41:55 +0330

    costume user done

--- accounts/models.py
+++ accounts/models.py
@@ -2,18 +2,18 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, AbstractBaseUser
 from .managers import UserManager
 
+
 class User(AbstractBaseUser):
     email = models.EmailField(max_length=225, unique=True)
     phone_number = models.CharField(max_length=11, unique=True)
-    full_name = models.CharField()
+    full_name = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
-
 
     object = UserManager()
 
     USERNAME_FIELD = 'phone_number'
-    REQUIRED_FIELDS = ['email']
+    REQUIRED_FIELDS = ['email', 'full_name']
 
     def __str__(self):
         return self.email
@@ -21,7 +21,7 @@ class User(AbstractBaseUser):
     def has_perm(self, perm, obj=None):
         return True
 
-    def has_module_perm(self, app_label):
+    def has_module_perms(self, app_label):
         return True
 
     @property
