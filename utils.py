b8d1a2fb65be77ce373578194c98f96d4429d268
Ghelyaei <ghelyaei97@gmail.com>
2023-01-22 17:35:47 +0330

    sms and phone number done

--- utils.py
+++ utils.py
@@ -1,3 +1,17 @@
+from kavenegar import *
 
-def send_top_code(phone_number, code):
-    pass
+
+def send_otp_code(phone_number, code):
+    try:
+        api = kavenegarAPI('6C69334F6B764F714559675A4F6C4A4C7949624A4646586B66426F6C2B547061384F446F473252553175513D')
+        params = {
+            'sender': '',
+            'receptor': phone_number,
+            'message': f'{code}کد تایید شما '
+        }
+        response = api.sms_send(params)
+        print(response)
+    except APIException as e:
+        print(e)
+    except HTTPException as e:
+        print(e)
