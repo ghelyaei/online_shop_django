e81c5ec46e32cffe802c8443cfa9f7d2655f3745
Ghelyaei <ghelyaei97@gmail.com>
2023-01-22 17:36:36 +0330

    sms and phone number done

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from kavenegar import *
 
 def send_otp_code(phone_number, code):
     try:
-        api = kavenegarAPI('6C69334F6B764F714559675A4F6C4A4C7949624A4646586B66426F6C2B547061384F446F473252553175513D')
+        api = kavenegarAPI('APIcode')
         params = {
             'sender': '',
             'receptor': phone_number,
